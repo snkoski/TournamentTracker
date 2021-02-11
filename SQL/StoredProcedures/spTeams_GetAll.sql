9e1c24589a3163e9d0b2deb42664a38e0df5300b
Shawn Koski <skoski@artnet.com>
2021-02-10 21:08:03 -0500

    Add spTeams_GetAll stored procedure.

---
+++ SQL/StoredProcedures/spTeams_GetAll.sql
@@ -0,0 +1,21 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spTeams_GetAll]
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT *
+	FROM [dbo].[Teams];
+END
+GO
