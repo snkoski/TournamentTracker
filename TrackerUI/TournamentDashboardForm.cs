f81080d056e1387e0a9cb9bbf34e20ac1cf6833c
Shawn Koski <skoski@artnet.com>
2021-02-20 10:42:50 -0500

    Populate TournamentDashboardForm with tournaments. Launch TournamentDashboardForm when starting program.

--- TrackerUI/TournamentDashboardForm.cs
+++ TrackerUI/TournamentDashboardForm.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
     public partial class TournamentDashboardForm : Form
     {
+        List<TournamentModel> existingTournaments = GlobalConfig.Connection.GetTournament_All();
         public TournamentDashboardForm()
         {
             InitializeComponent();
+
+            UpdateUi();
+        }
+
+        private void UpdateUi()
+        {
+            loadExistingTournamentDropDown.DataSource = existingTournaments;
+            loadExistingTournamentDropDown.DisplayMember = "TournamentName";
+        }
+        private void createTournamentButton_Click(object sender, EventArgs e)
+        {
+            CreateTournamentForm frm = new CreateTournamentForm();
+            frm.Show();
         }
     }
 }
