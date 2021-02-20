2db14cd162b14f2c49be872814c8813d3258cc73
Shawn Koski <skoski@artnet.com>
2021-02-20 10:04:33 -0500

    Add TeamCompetingId and ParentMatchupId to MatchupEntryModel. Add WinnerId to MatchupModel.

--- TrackerLibrary/Models/MatchupEntryModel.cs
+++ TrackerLibrary/Models/MatchupEntryModel.cs
@@ -12,14 +12,27 @@ namespace TrackerLibrary.Models
         /// The unique identifier for the matchup entry.
         /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The ID from the database that will be used to identify the team competing.
+        /// </summary>
+        public int TeamCompetingId { get; set; }
+
         /// <summary>
         /// Represents one team in the matchup.
         /// </summary>
         public TeamModel TeamCompeting { get; set; }
+
         /// <summary>
         /// Represents the score for this particular team.
         /// </summary>
         public double Score { get; set; }
+
+        /// <summary>
+        /// The ID from the database that will be used to identify the parent matchup.
+        /// </summary>
+        public int ParentMatchupId { get; set; }
+
         /// <summary>
         /// Represents the matchup that this team came frome as the winner.
         /// </summary>
--- TrackerLibrary/Models/MatchupModel.cs
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -15,14 +15,22 @@ namespace TrackerLibrary.Models
         /// The unique identifier for the matchup.
         /// </summary>
         public int Id { get; set; }
+
         /// <summary>
         /// The set of teams that were involved in this match.
         /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        /// <summary>
+        /// The ID from the database that will be used to identify the winner.
+        /// </summary>
+        public int WinnerId { get; set; }
+
         /// <summary>
         /// The winner of the match.
         /// </summary>
         public TeamModel Winner { get; set; }
+
         /// <summary>
         /// Which round this match is a part of.
         /// </summary>
